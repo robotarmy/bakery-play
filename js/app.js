0cbcdad48ad4fb92bcff4d451c3a389ab9edb055
Curtis9 <curtis@lindenlab.com>
2014-01-23 11:22:33 -0800

    search and sort

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  this.resource('bakery', {path:'/bakery'}, function() {
+  this.resource('bakery', {path:'/'}, function() {
     this.resource('goodies',{path:':bakery_name'})
   })
 });
@@ -25,8 +25,28 @@ App.BakeryController = Ember.ObjectController.extend({
 })
 
 App.GoodiesController = Ember.ArrayController.extend({
+  sortProperties:["name"],
+  sortAscending: true,
+
   needs: "bakery",
+  searchResults: Ember.computed.defaultTo("arrangedContent"),
+
+
+  filterItem: function (model) {
+    searchInput = this.get('controllers.bakery.searchText')
+    regexp = new RegExp(searchInput, "i");
+    if(!searchInput || (searchInput && (0 == searchInput.length))) {
+      return true
+    } else if (-1 != model.name.search(regexp)) {
+      return true
+    } else {
+      return false
+    }
+  },
+
   searchFilter: function() {
+    searchInput = this.get('controllers.bakery.searchText')
     Ember.Logger.debug("someone is looking for " + this.get("controllers.bakery.searchText"))
-  }.observes("controllers.bakery.searchText")
+    this.set('searchResults',this.get('arrangedContent').filter(this.filterItem.bind(this)))
+  }.observes("controllers.bakery.searchText"),
 })
