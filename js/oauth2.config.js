91926ee7cba96c08ee1941cd446ef29b50db9cbd
Curtis9 <curtis@lindenlab.com>
2014-01-31 14:57:44 -0800

    update scope setting

--- js/oauth2.config.js
+++ js/oauth2.config.js
@@ -3,7 +3,7 @@
       clientId: "334812751686-aa8lsb2ljoobderpk5plfokdads4qkbm.apps.googleusercontent.com",
       authBaseUri: 'https://accounts.google.com/o/oauth2/auth',
       redirectUri: 'https://bakery-play.herokuapp.com/oauth/callback',
-      scope: 'public write'
+      scope: 'openid profile email'
     } 
   } 
 
