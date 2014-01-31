227f80b62b039fc95ae4427443947493cdc07996
Curtis9 <curtis@lindenlab.com>
2014-01-31 14:14:31 -0800

    heroku static site

---
+++ js/oauth2.config.js
@@ -0,0 +1,8 @@
+  Ember.OAuth2.config = {
+    google: {
+      clientId: "xxxxxxxxxxxx",
+      authBaseUri: 'https://accounts.google.com/o/oauth2/auth',
+      redirectUri: 'https://oauth2-login-demo.appspot.com/oauth/callback',
+      scope: 'public write'
+    } 
+  } 
