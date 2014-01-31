21ce412fd5280e4c06e9d99431e6f412ce9f2656
Curtis9 <curtis@lindenlab.com>
2014-01-31 15:26:49 -0800

    oauth-callback

--- js/oauth2.config.js
+++ js/oauth2.config.js
@@ -2,7 +2,7 @@
     google: {
       clientId: "334812751686-aa8lsb2ljoobderpk5plfokdads4qkbm.apps.googleusercontent.com",
       authBaseUri: 'https://accounts.google.com/o/oauth2/auth',
-      redirectUri: 'https://bakery-play.herokuapp.com/oauth/callback',
+      redirectUri: 'https://bakery-play.herokuapp.com/oauth-callback',
       scope: 'openid profile email'
     } 
   } 
